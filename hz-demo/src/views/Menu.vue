5320f90f43bda955cbebd6f35833a4f54dc3d8ab
zhangming <shizuka@mocait.com>
2019-04-19 17:00:10 +0800

    update

--- hz-demo/src/views/Menu.vue
+++ hz-demo/src/views/Menu.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/terminology">医疗术语</router-link>|
-    <router-link to="/pagination">前端分页</router-link>
+    <router-link to="/terminology">医疗术语</router-link> |
+    <router-link to="/pagination">前端分页</router-link> | 
+    <router-link to="/eltree">el-tree根据type显示不同颜色</router-link>
   </div>
 </template>
